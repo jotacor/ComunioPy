e9ba031f202106b504427642f9ef98ec36abccd4
Javier Corbín <javi.corbin@gmail.com>
2014-11-03 22:34:27 +0100

    Comunio cambia como muestra los equipos por su icono. Recogemos texto alternativo del enlace.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from distutils.core import setup
 setup(
         name = 'ComunioPy',
         packages = ['ComunioPy'],
-        version = '1.0',
+        version = '1.0.1',
         description = 'API for comunio',
         license = 'MIT',
         long_description = open('README.md', 'rt').read(),
         author = 'Javier Corbín',
         author_email = 'javi.corbin@gmail.com',
         url = 'https://github.com/jotacor/ComunioPy',
-        download_url = 'https://github.com/jotacor/ComunioPy/archive/v1.0.tar.gz', 
+        download_url = 'https://github.com/jotacor/ComunioPy/archive/v1.0.1.tar.gz', 
         keywords = ['comunio', 'API'],
         classifiers = [],
 )
