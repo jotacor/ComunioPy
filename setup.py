5e498da30af56a69dd017463812f597b5e10ac07
Javier Corbín <javi.corbin@gmail.com>
2014-11-27 22:40:53 +0100

    Preparing setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from distutils.core import setup
+
+long_description = open('README.md', 'rt').read(),
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read(),
+        
 setup(
-        name = 'ComunioPy',
-        packages = ['ComunioPy'],
-        version = '1.0.1',
-        description = 'API for comunio',
-        license = 'MIT',
-        long_description = open('README.md', 'rt').read(),
-        author = 'Javier Corbín',
-        author_email = 'javi.corbin@gmail.com',
-        url = 'https://github.com/jotacor/ComunioPy',
-        download_url = 'https://github.com/jotacor/ComunioPy/archive/v1.0.1.tar.gz', 
-        keywords = ['comunio', 'API'],
-        classifiers = [],
+    name = 'ComunioPy',
+    packages = ['ComunioPy'],
+    version = '1.2',
+    description = 'API for comunio',
+    license = 'MIT',
+    author = 'Javier Corbín',
+    author_email = 'javi.corbin@gmail.com',
+    url = 'https://github.com/jotacor/ComunioPy',
+    download_url = 'https://github.com/jotacor/ComunioPy/archive/v1.2.tar.gz', 
+    keywords = ['comunio', 'API'],
+    classifiers = [],
 )
